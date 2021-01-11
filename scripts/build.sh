fa66446be1faaad0124c63c99bce3c0a8017f86f
hi1280 <hi12801280@gmail.com>
2021-01-11 20:55:49 +0900

    fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ do
   repository_uri=${REGISTRY_URI}/${container_name}
   aws ecr create-repository --repository-name ${container_name} --region ap-northeast-1 || true
   docker pull $repository_uri:latest || true
-  docker build --cache-from $repository_uri:latest --build-arg NODE_ENV=production --build-arg GITHUB_TOKEN=${GITHUB_TOKEN} --build-arg BUILDKIT_INLINE_CACHE=1 -t $repository_uri:$image_tag -f ${docker_filepath}/Dockerfile ${docker_filepath}
+  docker build --cache-from $repository_uri:latest --build-arg BUILDKIT_INLINE_CACHE=1 -t $repository_uri:$image_tag -f ${docker_filepath}/Dockerfile ${docker_filepath}
   image_digest=$(docker images $repository_uri --format "{{.Tag}}\t{{.Digest}}" | awk '$1 == "'"${image_tag}"'" {print $2}')
   if [ "${image_digest}" == "<none>" ]; then
     docker tag $repository_uri:$image_tag $repository_uri:latest
