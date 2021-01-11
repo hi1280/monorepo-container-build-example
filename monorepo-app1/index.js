758a89a736b7835fb59a87787b0af633fbfa364c
hi1280 <hi12801280@gmail.com>
2021-01-11 18:21:49 +0900

    fix: version up app1

--- monorepo-app1/index.js
+++ monorepo-app1/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('app1 ver 0.0.1 executed'));
+app.get('/', (req, res) => res.send('app1 ver 0.0.2 executed'));
 
 app.listen(3000, () => console.log('app1 listening on port 3000!'));
